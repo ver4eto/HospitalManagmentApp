6f252ca8d6878eb0569099fbabafd91eeb557503
ver4eto <vera.ruseva@gmail.com>
2024-10-30 17:18:26 +0200

    Add project files.

---
+++ HospitalManagmentApp/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace HospitalManagmentApp.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
