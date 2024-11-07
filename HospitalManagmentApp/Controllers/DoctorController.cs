2ec30d0d454dd9298892e56213e94b1b0b7b2517
ver4eto <vera.ruseva@gmail.com>
2024-11-07 18:34:17 +0200

    Doctor Controller and View for Index created

---
+++ HospitalManagmentApp/Controllers/DoctorController.cs
@@ -0,0 +1,34 @@
+﻿using HospitalManagment.ViewModels.Doctor;
+using HospitalManagmentApp.Data;
+using HospitalManagmentApp.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using System.Data.Entity;
+
+namespace HospitalManagmentApp.Controllers
+{
+    public class DoctorController : Controller
+    {
+        private readonly HMDbContext context;
+
+        public DoctorController(HMDbContext _context)
+        {
+            this.context = _context;
+        }
+        public IActionResult Index()
+        {
+            var doctors=context
+                .Doctors                
+                .Select(d=>new DoctorIndexViewModel()
+                {
+                   FirstName= d.FirstName,
+                  LastName=  d.LastName,
+                   Specialty= d.Specialty,
+                   DepartmentName= d.Department.Name,
+
+                })
+                .ToArray()
+                ;
+            return View();
+        }
+    }
+}
