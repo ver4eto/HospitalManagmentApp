e82da7d0a3d6d963dfa52fd4ac763d977857c149
ver4eto <vera.ruseva@gmail.com>
2024-11-08 11:16:28 +0200

    Nurse controller and Index page created

---
+++ HospitalManagment.ViewModels/Nurse/NurseIndexViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagment.ViewModels.Nurse
+{
+    public class NurseIndexViewModel
+    {
+       
+        public string FirstName { get; set; } = null!;
+
+
+      
+        public string LastName { get; set; } = null!;
+
+        public string Department { get; set; } = null!;
+    }
+}
---
+++ HospitalManagmentApp/Controllers/NurseController.cs
@@ -0,0 +1,32 @@
+﻿using HospitalManagment.ViewModels.Nurse;
+using HospitalManagmentApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagmentApp.Controllers
+{
+    public class NurseController : Controller
+    {
+        private readonly HMDbContext context;
+
+        public NurseController(HMDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task< IActionResult> Index()
+        {
+            var nurses=await context
+                .Nurses
+                .Where(n=>n.IsDeleted==false)
+                .Select(n =>new NurseIndexViewModel
+                { 
+                FirstName = n.FirstName,
+                LastName = n.LastName,
+                Department=n.Department.Name,
+                })
+                .ToListAsync();
+
+            return View(nurses);
+        }
+    }
+}
