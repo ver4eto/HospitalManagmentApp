c517de09ce6f2b6fd2d1bbeede43772f230c8e2f
ver4eto <vera.ruseva@gmail.com>
2024-11-08 12:28:59 +0200

    Patient controller and index created

---
+++ HospitalManagment.ViewModels/Patient/PatientIndexViewModel.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagment.ViewModels.Patient
+{
+    public class PatientIndexViewModel
+    {
+        
+        public string Name { get; set; } = null!;
+                
+        
+        public string EGN { get; set; }=null!;
+      
+        public string EmailAddress { get; set; } = null!;
+
+
+        public string PhoneNumber { get; set; } = null!;
+
+       
+        public string Address { get; set; } = null!;
+
+
+        public string HasMedicalInsurance { get; set; } = null!;
+
+        public string Department { get; set; }=null!;
+        public int Room { get; set; }
+    }
+}
---
+++ HospitalManagmentApp/Controllers/PatientController.cs
@@ -0,0 +1,50 @@
+﻿using HospitalManagment.ViewModels.Patient;
+using HospitalManagmentApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagmentApp.Controllers
+{
+
+    public class PatientController : Controller
+    {
+
+        private readonly HMDbContext context;
+
+        public PatientController(HMDbContext context)
+        {
+            this.context = context;
+        }
+        public async Task<IActionResult> Index()
+        {
+            var patients = await context
+                .Patients
+                .Where(p => p.IsDeleted == false)
+                .Select(p => new PatientIndexViewModel()
+                {
+                    Name = $"{p.FirstName} {p.LastName}",
+                    EGN = p.EGN,
+                    PhoneNumber = p.PhoneNumber,
+                    Address = p.Address,
+                    Department = p.Department.Name,
+                    Room = p.Room.RoomNumber,
+                    HasMedicalInsurance = HasMedicalInsurance(p.HasMedicalInsurance),
+                    EmailAddress = p.EmailAddress,
+
+                })
+                .ToListAsync();
+
+            return View(patients);
+        }
+
+        private static string HasMedicalInsurance(bool hasMedicalInsurance)
+        {
+            if (hasMedicalInsurance==true)
+            {
+                return "Yes";
+            }
+            return "No";
+
+        }
+    }
+}
