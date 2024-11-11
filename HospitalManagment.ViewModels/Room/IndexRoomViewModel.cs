1ee87491970488a0d0f62b5a6076b8103a1009d8
ver4eto <vera.ruseva@gmail.com>
2024-11-11 17:35:54 +0200

    Added room to home controller, added room view method

---
+++ HospitalManagment.ViewModels/Room/IndexRoomViewModel.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HospitalManagmentApp.DataModels;
+
+namespace HospitalManagment.ViewModels.Room
+{
+    public class IndexRoomViewModel
+    {
+       
+        public Guid Id { get; set; }
+
+       
+        public int RoomNumber { get; set; }
+
+        
+        public int BedCount { get; set; }
+
+      
+        public string HasFreeBeds { get; set; } = null!;
+
+        public string Departmnet { get; set; } = null!;       
+
+        
+
+    }
+}
---
+++ HospitalManagmentApp/Controllers/RoomController.cs
@@ -0,0 +1,44 @@
+﻿using HospitalManagment.ViewModels.Nurse;
+using HospitalManagment.ViewModels.Room;
+using HospitalManagmentApp.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagmentApp.Controllers
+{
+    public class RoomController : Controller
+    {
+        private readonly HMDbContext context;
+        public RoomController(HMDbContext hMDb )
+        {
+            this.context = hMDb;
+        }
+        public async Task< IActionResult> Index()
+        {
+            var rooms = await context
+               .Rooms
+               .Where(n => n.IsDeleted == false)
+               .Select(n => new IndexRoomViewModel
+               {
+                   Id = n.Id,
+                   BedCount = n.BedCount,
+                   RoomNumber = n.RoomNumber,
+                   Departmnet = n.Department.Name,
+                   HasFreeBeds=HasFreeBeds(n.HasFreeBeds)
+               })
+               .ToListAsync();
+
+            return View(rooms);
+           
+        }
+
+        private static string HasFreeBeds(bool hasFreeBeds)
+        {
+            if (hasFreeBeds)
+            {
+                return "Yes!";
+            }
+            return "No";
+        }
+    }
+}
