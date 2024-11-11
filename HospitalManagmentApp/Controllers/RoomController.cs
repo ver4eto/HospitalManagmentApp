2b332ce5f17df9cf4cb2b0e126a6698fb02dd9b2
ver4eto <vera.ruseva@gmail.com>
2024-11-11 18:24:06 +0200

    Add room methid added

--- HospitalManagmentApp/Controllers/RoomController.cs
+++ HospitalManagmentApp/Controllers/RoomController.cs
@@ -1,6 +1,7 @@
 ﻿using HospitalManagment.ViewModels.Nurse;
 using HospitalManagment.ViewModels.Room;
 using HospitalManagmentApp.Data;
+using HospitalManagmentApp.DataModels;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
@@ -9,11 +10,11 @@ namespace HospitalManagmentApp.Controllers
     public class RoomController : Controller
     {
         private readonly HMDbContext context;
-        public RoomController(HMDbContext hMDb )
+        public RoomController(HMDbContext hMDb)
         {
             this.context = hMDb;
         }
-        public async Task< IActionResult> Index()
+        public async Task<IActionResult> Index()
         {
             var rooms = await context
                .Rooms
@@ -24,13 +25,52 @@ namespace HospitalManagmentApp.Controllers
                    BedCount = n.BedCount,
                    RoomNumber = n.RoomNumber,
                    Departmnet = n.Department.Name,
-                   HasFreeBeds=HasFreeBeds(n.HasFreeBeds)
+                   HasFreeBeds = HasFreeBeds(n.HasFreeBeds)
                })
                .ToListAsync();
 
             return View(rooms);
-           
+
         }
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var room = new AddRoomViewModel();
+            room.Departments = await GetDepartments();
+            return View(room);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddRoomViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                model.Departments = await GetDepartments();
+                return View(model);
+            }
+            var room = await context.Rooms
+                .Where(r=>r.DepartmnetId==model.DepartmnetId && r.RoomNumber==model.RoomNumber)
+                .FirstOrDefaultAsync();
+
+            if (room != null)
+            {
+                ModelState.AddModelError(string.Empty, "This room already exists in the current department.");
+                return View(model);
+            }
+
+           room = new Room()
+            {
+                RoomNumber = model.RoomNumber,
+                BedCount = model.BedCount,
+                DepartmnetId = model.DepartmnetId,
+            };
+
+
+            await context.Rooms.AddAsync(room);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
 
         private static string HasFreeBeds(bool hasFreeBeds)
         {
@@ -39,6 +79,11 @@ namespace HospitalManagmentApp.Controllers
                 return "Yes!";
             }
             return "No";
+        }
+
+        private async Task<ICollection<Department>> GetDepartments()
+        {
+            return await context.Departments.ToListAsync();
         }
     }
 }
