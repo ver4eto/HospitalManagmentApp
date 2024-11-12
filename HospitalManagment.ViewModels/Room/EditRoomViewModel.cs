069b2a81af3ea48fd7fe11f0831071a7c70c8521
ver4eto <vera.ruseva@gmail.com>
2024-11-12 13:38:42 +0200

    Edit room method added

---
+++ HospitalManagment.ViewModels/Room/EditRoomViewModel.cs
@@ -0,0 +1,30 @@
+﻿using HospitalManagmentApp.DataModels;
+using System.ComponentModel.DataAnnotations;
+using static HospitalManagmentApp.Common.ApplicationConstants;
+using static HospitalManagmentApp.Common.EntityValidationConstants.Room;
+
+
+namespace HospitalManagment.ViewModels.Room
+{
+    public class EditRoomViewModel
+    {       
+        public Guid Id { get; set; }
+
+        [Required(ErrorMessage = RequiredErrorMessage)]
+        //range???
+        public int RoomNumber { get; set; }
+
+        [Required(ErrorMessage =RequiredErrorMessage)]
+        [Range(MinBedCount, MaxBedCount)]        
+        public int BedCount { get; set; }
+
+        //[Required(ErrorMessage = RequiredErrorMessage)]
+        //[Comment("Does room has free beds?")]
+        //public bool HasFreeBeds { get; set; } = true;
+      
+        
+        public Guid DepartmnetId { get; set; }    
+        public IEnumerable<Department> Departments { get; set; } = new List<Department>();
+    }
+}
+
--- HospitalManagmentApp/Controllers/RoomController.cs
+++ HospitalManagmentApp/Controllers/RoomController.cs
@@ -48,9 +48,8 @@ namespace HospitalManagmentApp.Controllers
                 model.Departments = await GetDepartments();
                 return View(model);
             }
-            var room = await context.Rooms
-                .Where(r=>r.DepartmnetId==model.DepartmnetId && r.RoomNumber==model.RoomNumber)
-                .FirstOrDefaultAsync();
+
+            var room = await CheckIfSameNumberRoomExistInCurrentDepartment(model.DepartmnetId, model.RoomNumber);
 
             if (room != null)
             {
@@ -71,6 +70,74 @@ namespace HospitalManagmentApp.Controllers
             return RedirectToAction(nameof(Index));
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Edit(Guid id)
+        {
+            var room = await context
+                .Rooms
+                .Where(d => d.Id == id && d.IsDeleted == false)
+                .Include(d => d.Department)
+                .FirstOrDefaultAsync();
+
+            if (room == null)
+            {               
+               return BadRequest();
+            }
+
+            EditRoomViewModel model = new()
+            {
+                Id = room.Id,
+                RoomNumber = room.RoomNumber,
+                BedCount = room.BedCount,
+                DepartmnetId=room.DepartmnetId,
+                Departments = await GetDepartments()
+            };
+
+            return View(model);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Edit(EditRoomViewModel model, Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+
+                return View(model);
+            }
+
+            var room = await context
+                .Rooms
+                .FindAsync(id);
+
+            if (room == null)
+            {
+                ModelState.AddModelError(string.Empty, "No such room exists in the current department.");
+                return View(model);
+                //return BadRequest();
+            }                       
+
+            //if (await CheckIfSameNumberRoomExistInCurrentDepartment(model.DepartmnetId,model.RoomNumber) != null)
+            //{
+            //    ModelState.AddModelError(string.Empty, "This room already exists in the current department.");
+            //    return View(model);
+            //}
+
+            room.RoomNumber = model.RoomNumber;
+            room.BedCount = model.BedCount;            
+            room.DepartmnetId = model.DepartmnetId;
+
+
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+
+        }
+
+        private async Task<Room?> CheckIfSameNumberRoomExistInCurrentDepartment(Guid id, int roomNumber)
+        {
+            return await context.Rooms
+                          .Where(r => r.DepartmnetId == id && r.RoomNumber == roomNumber)
+                          .FirstOrDefaultAsync();
+        }
 
         private static string HasFreeBeds(bool hasFreeBeds)
         {
