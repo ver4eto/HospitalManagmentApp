eb78386ab12f97181bfe2eded06c2bda7562d8d9
ver4eto <vera.ruseva@gmail.com>
2024-11-14 16:09:57 +0200

    Add new treatment method added

---
+++ HospitalManagmentApp/Controllers/TreatmentController.cs
@@ -0,0 +1,60 @@
+﻿using HospitalManagment.ViewModels.Nurse;
+using HospitalManagment.ViewModels.Treatment;
+using HospitalManagmentApp.Data;
+using HospitalManagmentApp.DataModels;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace HospitalManagmentApp.Controllers
+{
+    public class TreatmentController : Controller
+    {
+        private readonly HMDbContext context;
+
+        public TreatmentController(HMDbContext dbContext)
+        {
+            this.context = dbContext;
+        }
+        public async Task< IActionResult> Index()
+        {
+            var treatments=  await context
+                .Treatments
+                .Where(t=>t.IsDeleted==false)
+                .Select(t=>new TreatmentIndexViewModel()
+                {
+                    Name = t.Name,
+                    Price=t.Price.ToString("f2"),
+                    Id=t.Id
+                })
+                .ToListAsync();
+            return View(treatments);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Add()
+        {
+            var treatment = new AddTreatmentViewModel();            
+            return View(treatment);
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Add(AddTreatmentViewModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+               
+                return View(model);
+            }
+
+            var treatment = new Treatment()
+            {
+                Name=model.Name,
+                Price=model.Price,
+            };
+
+            await context.Treatments.AddAsync(treatment);
+            await context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+    }
+}
