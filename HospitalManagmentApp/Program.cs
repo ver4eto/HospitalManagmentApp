6a43682417f375cc8dd6b0c681cd9d151ac957c1
ver4eto <vera.ruseva@gmail.com>
2024-11-21 18:11:13 +0200

    Register the repository in Program.cs

--- HospitalManagmentApp/Program.cs
+++ HospitalManagmentApp/Program.cs
@@ -1,10 +1,14 @@
+using HospitalManagment.Infrastructure.Repositories;
+using HospitalManagment.Infrastructure.Repositories.Contracts;
+using NuGet.Protocol.Core.Types;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApplicationDbContext(builder.Configuration);
 builder.Services.AddApplicationIdentity(builder.Configuration);
 
 builder.Services.AddControllersWithViews();
-
+builder.Services.AddScoped(typeof(IRepository<>), typeof(Repository<>));
 builder.Services.AddApplicationServices();
 
 var app = builder.Build();
