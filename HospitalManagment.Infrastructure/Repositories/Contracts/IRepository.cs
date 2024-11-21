414931350cc89916d5f1b431ca643714d8ec2927
ver4eto <vera.ruseva@gmail.com>
2024-11-21 16:16:53 +0200

    Repository added

---
+++ HospitalManagment.Infrastructure/Repositories/Contracts/IRepository.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagment.Infrastructure.Repositories.Contracts
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+
+        Task<T> GetByIdAsync(Guid id);
+
+        Task AddAsync(T entity);
+
+        Task UpdateAsync(T entity);
+        Task DeleteAsync(Guid id);
+
+    }
+}
---
+++ HospitalManagment.Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,60 @@
+﻿using HospitalManagment.Infrastructure.Repositories.Contracts;
+using HospitalManagmentApp.Data;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HospitalManagment.Infrastructure.Repositories
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly HMDbContext dbContext;
+        private readonly DbSet<T> dbSet;
+
+        public Repository(HMDbContext context)
+        {
+            this.dbContext = context;
+            this.dbSet = context.Set<T>();
+        }
+        public async Task AddAsync(T entity)
+        {
+            await dbSet.AddAsync(entity);
+            await dbContext.SaveChangesAsync();
+
+        }
+
+        public async Task DeleteAsync(Guid id)
+        {
+            var entity = await GetByIdAsync(id);
+            if (entity != null)
+            {
+                dbSet.Remove(entity);
+                await dbContext.SaveChangesAsync();
+            }
+
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await dbSet.ToListAsync();
+
+        }
+
+        public async Task<T> GetByIdAsync(Guid id)
+        {
+            return await dbSet.FindAsync(id);
+
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            dbSet.Attach(entity);
+            dbContext.Entry(entity).State = EntityState.Modified;
+            await dbContext.SaveChangesAsync();
+
+        }
+    }
+}
