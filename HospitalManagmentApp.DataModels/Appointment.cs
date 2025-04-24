83c7e496e61dab1557cafcfd85c28f1df30ec449
ver4eto <vera.ruseva@gmail.com>
2025-04-24 10:25:35 +0300

    Appointment class added, so it will be posiible to make appointments for different doctors

---
+++ HospitalManagmentApp.DataModels/Appointment.cs
@@ -0,0 +1,14 @@
+﻿
+namespace HospitalManagmentApp.DataModels
+{
+    public class Appointment
+    {
+        public Guid Id { get; init; } = Guid.NewGuid();
+        public Doctor Doctor { get; init; } = default!;
+        public Patient Patient { get; init; } = default!;
+        public DateTime Start { get; init; }
+        public DateTime End { get; init; }
+
+        public override string ToString() => $"{Start:yyyy-MM-dd HH:mm} – {End:HH:mm} | Dr. {Doctor.FirstName} {Doctor.LastName} with {Patient.FirstName} {Patient.LastName}";
+    }
+}
